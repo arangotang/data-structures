b88d1a1f11cb65290f95704f215f9b547eb3fb5b
Andres Arango <ararango@comcast.net>
2022-04-13 16:43:53 -0700

    pass all tests for es6 stack and queue

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,29 @@
 class Queue {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.count = 0;
   }
 
+  enqueue(value) {
+    this.storage[this.count] = value;
+    this.count++;
+  }
+
+  dequeue() {
+    if (this.count === 0) {
+      return;
+    }
+
+    const dequeued = this.storage['0'];
+    for (let i = 0; i < this.count - 1; i++) {
+      this.storage[i] = this.storage[i + 1];
+    }
+    delete this.storage[this.count - 1];
+    this.count--;
+    return dequeued;
+  }
+
+  size() {
+    return this.count;
+  }
 }
