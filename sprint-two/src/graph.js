f027a0b7fe824eefe94fb006939c6ed7cb7e4c31
Andres Arango <ararango@comcast.net>
2022-04-14 13:22:41 -0700

    pass all tests for graph (pseudoclassical)

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,5 +1,3 @@
-
-
 // Instantiate a new graph
 var Graph = function() {
   this.nodes = {};
@@ -21,36 +19,48 @@ Graph.prototype.contains = function(node) {
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
-  // if node is not contained in graph
-    // return
-
-  // iterate through this.nodes
-    // if current node has value of node
-      // splice out of the array
-    // otherwise
-      // iterate through currentNode's connections
-        // if connections contains a node of value
+  if (!this.contains(node)) {
+    return;
+  }
+  delete this.nodes[node];
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  if (!this.contains(fromNode) || !this.contains(toNode)) {
+    return false;
+  }
+  let fromHasTo = this.nodes[fromNode].connections[toNode] !== undefined;
+  let toHasFrom = this.nodes[toNode].connections[fromNode] !== undefined;
+  return fromHasTo && toHasFrom;
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  this.nodes[fromNode].connections[toNode] = this.nodes[toNode];
+  this.nodes[toNode].connections[fromNode] = this.nodes[fromNode];
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  delete this.nodes[fromNode].connections[toNode];
+  delete this.nodes[toNode].connections[fromNode];
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  for (let node in this.nodes) {
+    cb(node);
+  }
 };
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * addNode: O(1) - independent of number of nodes in graph
+ * contains: O(1) - independent of number of nodes in graph
+ * removeNode: O(1) - independent of number of nodes in graph
+ * hasEdge: O(1) - independent of number of nodes in graph
+ * addEdge: O(1) - independent of number of nodes in graph
+ * removeEdge: O(1) - independent of number of nodes in graph
+ * forEachNode: O(n) - iterates through each on n nodes in graph
  */
-
-
