1c61aaa21c5c62aa541c7623021402a15930333e
Andres Arango <ararango@comcast.net>
2022-04-14 12:06:00 -0700

    pass all tests and answer Qs for tree (functional shared)

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,8 +2,8 @@ var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
 
-  // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -11,13 +11,27 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  let newChild = Tree(value);
+  this.children.push(newChild);
 };
 
 treeMethods.contains = function(target) {
+  if (this.value === target) {
+    return true;
+  }
+
+  let containsTarget = false;
+
+  for (let i = 0; i < this.children.length; i++) {
+    containsTarget = containsTarget || this.children[i].contains(target);
+  }
+  return containsTarget;
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * addChild: O(1) - same operation regardless of input / tree size
+ * contains: O(n) - worst case: have to recurse through every child to find if its value matches target
  */
