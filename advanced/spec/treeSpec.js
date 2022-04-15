74405b19491ecdba557ad57198a9e16a44731960
Andres Arango <ararango@comcast.net>
2022-04-15 15:08:32 -0700

    implemented and successfully tested traverse method for tree

--- advanced/spec/treeSpec.js
+++ advanced/spec/treeSpec.js
@@ -5,21 +5,22 @@ describe('tree w/ parent', function() {
     tree = Tree();
   });
 
-  it('should have methods named "addChild", "contains", and "removeFromParent", and properties named "value" and "parent"', function() {
+  it('should have methods named "addChild", "contains", "removeFromParent", and "traverse", and properties named "value" and "parent"', function() {
     expect(tree.addChild).to.be.a('function');
     expect(tree.contains).to.be.a('function');
     expect(tree.removeFromParent).to.be.a('function');
+    expect(tree.traverse).to.be.a('function');
     expect(tree.hasOwnProperty('value')).to.equal(true);
     expect(tree.hasOwnProperty('parent')).to.equal(true);
   });
 
-  it('should return null of topmost node', function() {
+  it('should return null for parent of topmost tree', function() {
     expect(tree.parent).to.equal(null);
   });
 
   it('should refer to parent node of a child tree', function() {
     tree.addChild(5);
-    expect(tree.children[0].parent).to.not.equal(tree);
+    expect(tree.children[0].parent).to.equal(tree);
   });
 
   it('should add children to the tree', function() {
@@ -59,6 +60,47 @@ describe('tree w/ parent', function() {
     tree.children[1].addChild(8);
     expect(tree.contains('7')).to.equal(false);
     expect(tree.contains('8')).to.equal(false);
+  });
+
+  it('should should remove child from parent', function() {
+    tree.addChild(5);
+    tree.children[0].removeFromParent();
+    expect(tree.contains(5)).to.equal(false);
+  });
+
+  it('should parentless return child when removed from parent', function() {
+    tree.addChild(5);
+    tree.children[0].addChild(6);
+    tree.children[0].addChild(7);
+    let childCopy = Tree(5);
+    childCopy.addChild(6);
+    childCopy.addChild(7);
+    expect(tree.children[0].removeFromParent()).to.eql(childCopy);
+    expect(childCopy.parent).to.equal(null);
+  });
+
+  it('should not contain former child nor its children after being removed', function() {
+    tree.addChild(5);
+    tree.children[0].addChild(6);
+    tree.children[0].addChild(7);
+    tree.children[0].removeFromParent();
+    expect(tree.contains(6)).to.equal(false);
+    expect(tree.contains(7)).to.equal(false);
+    expect(tree.contains(5)).to.equal(false);
+  });
+
+  it('should accept a callback and execute it on every value contained in the tree', function() {
+    tree = Tree(4);
+    tree.addChild(5);
+    tree.children[0].addChild(6);
+    tree.children[0].children[0].addChild(9);
+    tree.children[0].addChild(7);
+    let array = [];
+    const func = function(value) {
+      array.push(value);
+    };
+    tree.traverse(func);
+    expect(array).to.eql([4, 5, 6, 9, 7]);
   });
 
 });
--- advanced/src/tree.js
+++ advanced/src/tree.js
@@ -1,7 +1,8 @@
-const Tree = function(value) {
+const Tree = function(value, parent) {
   let newTree = {};
   newTree.value = value;
   newTree.children = [];
+  newTree.parent = parent || null;
   _.extend(newTree, treeMethods);
 
   return newTree;
@@ -9,8 +10,8 @@ const Tree = function(value) {
 
 var treeMethods = {};
 
-treeMethods.addChild = function(value) {
-  let newChild = Tree(value);
+treeMethods.addChild = function(value, parent) {
+  let newChild = Tree(value, this);
   this.children.push(newChild);
 };
 
@@ -23,6 +24,38 @@ treeMethods.contains = function(target) {
   }
 
   return containsTarget;
+};
+
+treeMethods.removeFromParent = function() {
+  // store this on a variable
+  let orphan = this;
+  // remove child from its parent's children array
+  // iterate through child's parent's children array
+  for (let i = 0; i < this.parent.children.length; i++) {
+    // if current child has the same value as stored child
+    if (this.parent.children[i].value === this.value) {
+      // splice out of array
+      this.parent.children.splice(i, 1);
+      // break
+      break;
+    }
+  }
+  // set parent value on variable to null
+  orphan.parent = null;
+  // return variable
+  return orphan;
+};
+
+treeMethods.traverse = function(cb) {
+  cb(this.value);
+  // check if it has children
+  if (this.children.length > 0) {
+    // iterate through children
+    for (let i = 0; i < this.children.length; i++) {
+      // recurse on child
+      this.children[i].traverse(cb);
+    }
+  }
 };
 
 /*
