d5a41b418d31c99abde6e299795e7cc64b205c08
Andres Arango <ararango@comcast.net>
2022-04-14 20:04:49 -0700

    added additional test for sets

--- sprint-two/spec/setSpec.js
+++ sprint-two/spec/setSpec.js
@@ -24,4 +24,8 @@ describe('set', function() {
     expect(set.contains('Mel Gibson')).to.equal(false);
   });
 
+  it('should not contain values that were never in the set', function() {
+    expect(set.contains('Mel Gibson')).to.equal(false);
+  });
+
 });
