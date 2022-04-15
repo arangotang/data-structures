62f9c31d4a67a89284ab1759da8c19ef340f5cb6
Andres Arango <ararango@comcast.net>
2022-04-15 11:06:16 -0700

    began advanced tree section

---
+++ advanced/src/tree.js
@@ -0,0 +1,32 @@
+const Tree = function(value) {
+  let newTree = {};
+  newTree.value = value;
+  newTree.children = [];
+  _.extend(newTree, treeMethods);
+
+  return newTree;
+};
+
+var treeMethods = {};
+
+treeMethods.addChild = function(value) {
+  let newChild = Tree(value);
+  this.children.push(newChild);
+};
+
+treeMethods.contains = function(target) {
+  if (this.value === target) { return true; }
+
+  let containsTarget = false;
+  for (let i = 0; i < this.children.length; i++) {
+    containsTarget = containsTarget || this.children[i].contains(target);
+  }
+
+  return containsTarget;
+};
+
+/*
+ * Complexity: What is the time complexity of the above functions?
+ * addChild: O(1) - same operation regardless of input / tree size
+ * contains: O(n) - worst case: have to recurse through every child to find if its value matches target
+ */
