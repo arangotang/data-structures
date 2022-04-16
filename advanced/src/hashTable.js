278ac605cf25be6bb2c47e8a659d58cc5fef20d3
Andres Arango <ararango@comcast.net>
2022-04-15 18:46:38 -0700

    passed both advanced tests for hash table

--- advanced/src/hashTable.js
+++ advanced/src/hashTable.js
@@ -6,13 +6,17 @@ const HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   let index = this._storage.getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(index, v);
+  this._storage.set(index, [k, v]);
   this._count++;
+  this.checkSize();
 };
 
 HashTable.prototype.retrieve = function(k) {
   let index = this._storage.getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(index);
+  if (this._storage.get(index) === undefined) {
+    return;
+  }
+  return this._storage.get(index)[1];
 };
 
 HashTable.prototype.remove = function(k) {
@@ -23,6 +27,29 @@ HashTable.prototype.remove = function(k) {
     }
   });
   this._count--;
+  this.checkSize();
+};
+
+HashTable.prototype.checkSize = function() {
+  let isTooSmall = this._count >= 0.75 * this._limit;
+  let isTooBig = this._count < 0.25 * this._limit;
+  if (isTooBig || isTooSmall) {
+    let newLimit;
+    if (isTooSmall) {
+      newLimit = 2 * this._limit;
+    } else {
+      newLimit = 0.5 * this._limit;
+    }
+    var newStorage = LimitedArray(newLimit);
+    this._storage.each(function(item, i, collection) {
+      if (item !== undefined) {
+        let newIndex = newStorage.getIndexBelowMaxForKey(item[0], newLimit);
+        newStorage.set(newIndex, [item[0], item[1]]);
+      }
+    });
+    this._limit = newLimit;
+    this._storage = newStorage;
+  }
 };
 
 /*
@@ -30,6 +57,7 @@ HashTable.prototype.remove = function(k) {
  * insert: O(n) - uses 'getIndexBelowMaxForKey' (O(n)) and 'set' (O(1)) helper functions, while the remainder of this function is O(1).
  * retrieve: O(n) - uses 'getIndexBelowMaxForKey' (O(n)) and 'get' (O(1)) helper functions, while the remainder of this function is O(1).
  * remove: O(n) - uses 'getIndexBelowMaxForKey' and 'each' helper functions which are both O(n), while the remainder of this function is constant time.
+ * checkSize: O(n) - iterates through everything in the list to re-populate new ht
  */
 
 
